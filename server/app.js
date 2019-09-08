de3884a26d7fb9532baa949b9e3d7a3e7a7cb738
charouf nassro <charoufnassro@gmail.com>
2019-09-08 21:36:40 +0200

    final code with error

--- server/app.js
+++ server/app.js
@@ -8,7 +8,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
 
+// Add Graphql and mongoose db
+
+var graphqlHTTP = require('express-graphql');
+var schema = require('./graphql/bookSchemas');
+var cors = require("cors");
+
 var app = express();
+
+app.use('*', cors());
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: schema,
+  rootValue: global,
+  graphiql: true,
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +52,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 mongoose.connect('mongodb://localhost/node-graphql', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
   .then(() =>  console.log('connection successful'))
